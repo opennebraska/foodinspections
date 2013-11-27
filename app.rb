c8eef1e149d0b1b0599ef21997939cb6fa6f35c9
Ross Nelson <ross.nelson@gmail.com>
2013-11-27 08:18:27 -0600

    Removed commented out :port config in app.rb

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ require 'net/http'
 require 'sass'
 Bundler.require
 
-#set :port, 80
-
 # get '/css/*.css' do
 # 	content_type 'text/css', :charset => 'utf-8'
 # 	scss params[:splat].join.to_sym, :style => :compressed
