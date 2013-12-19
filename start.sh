ec2d8ff834fe5a3a33a46d6db48839b99178ee4a
Ross Nelson <ross.nelson@gmail.com>
2013-12-19 08:34:28 -0600

    Made start.sh check for and source the RVM init script

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+if [ -x ~/.rvm/scripts/rvm ]; then
+  source ~/.rvm/scripts/rvm
+else
+  if [ -x /usr/local/rvm/scripts/rvm ]; then
+    source /usr/local/rvm/scripts/rvm
+  fi
+fi
+
 unicorn -c config/unicorn.rb -D -E production
