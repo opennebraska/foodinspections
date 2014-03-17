d7d3ba8b0e2e007229f112f834ebd3b018cc7d1a
Ross Nelson <ross.nelson@gmail.com>
2014-03-17 09:48:48 -0500

    Changed .ruby-version and the vagrant setup to use Ruby 2.1.1

--- setup/vagrant/shell/ruby.sh
+++ setup/vagrant/shell/ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "if [ ! -f ~/.installedrvm ]; then" >> ~vagrant/.profile
-echo "  echo 'Installing RVM and Ruby 2.1'" >> ~vagrant/.profile
-echo "  curl -L https://get.rvm.io | bash -s stable --auto-dotfiles >/dev/null && source ~/.rvm/scripts/rvm && rvm install --default 2.1 >/dev/null && gem install bundler >/dev/null" >> ~vagrant/.profile
+echo "  echo 'Installing RVM and Ruby 2.1.1'" >> ~vagrant/.profile
+echo "  curl -L https://get.rvm.io | bash -s stable --auto-dotfiles >/dev/null && source ~/.rvm/scripts/rvm && rvm install --default 2.1.1 >/dev/null && gem install bundler >/dev/null" >> ~vagrant/.profile
 echo "  echo 'Installing gems for Food Inspection'" >> ~vagrant/.profile
 echo "  cd /src && bundle install >/dev/null" >> ~vagrant/.profile
 echo "  echo 'Gems installed'" >> ~vagrant/.profile
